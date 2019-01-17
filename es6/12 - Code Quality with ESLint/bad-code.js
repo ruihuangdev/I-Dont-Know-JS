28d1c2620796e7428f5f8abea9cd13ac5d7c914b
HR <hrui1990@gmail.com>
2019-01-17 00:35:08 -0600

    eslint day 2

--- es6/12 - Code Quality with ESLint/bad-code.js
+++ es6/12 - Code Quality with ESLint/bad-code.js
@@ -1,3 +1,4 @@
+/* globals twttr ga */
 const weather = new Promise((resolve) => {
   setTimeout(() => {
     resolve({ temp: 29, conditions: 'Sunny with Clouds' });
@@ -25,3 +26,48 @@ Promise.all([postsPromise, streetCarsPromise])
   .then((responses) => {
     console.log(responses);
   });
+
+// declare global vars which eslint will complain if not declared at the beginning of the file
+
+ga.track();
+twttr.trackConversion();
+
+// an array method polyfill that eslint will complain if we do not disable certain rules
+
+/* eslint-disable */
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, "includes", {
+    value(valueToFind, fromIndex) {
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      const o = Object(this);
+      const len = o.length >>> 0;
+
+      if (len === 0) {
+        return false;
+      }
+      const n = fromIndex | 0;
+      let k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      function sameValueZero(x, y) {
+        return (
+          x === y ||
+          (typeof x === "number" &&
+            typeof y === "number" &&
+            isNaN(x) &&
+            isNaN(y))
+        );
+      }
+      while (k < len) {
+        if (sameValueZero(o[k], valueToFind)) {
+          return true;
+        }
+        k++;
+      }
+      return false;
+    }
+  });
+}
+/* eslint-enable */
