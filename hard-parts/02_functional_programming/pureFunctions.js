811268f77d575dbeac12d4daa426eb303f5e9575
HR <hrui1990@gmail.com>
2019-03-06 23:49:39 -0600

    functional programming

--- hard-parts/02_functional_programming/pureFunctions.js
+++ hard-parts/02_functional_programming/pureFunctions.js
@@ -1,4 +1,4 @@
 // function programming is a paradigm (just like OOP)
 // some core features of functional programming
 // 1. Pure functions (no side effects)
-// 2. 'Higher order functions' - highly valuable tool
+// 2. 'Higher order functions' - a highly valuable tool
