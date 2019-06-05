9df4a5ea5416af78304200825313e5c3e2f10092
HR <hrui1990@gmail.com>
2019-06-04 23:44:03 -0500

    update project 4

--- data-and-api/project4/index.js
+++ data-and-api/project4/index.js
@@ -4,3 +4,23 @@ const app = express();
 app.listen(3000, () => console.log("Listening on port 3000"));
 
 app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.post("/api", (req, res) => {
+  console.log(req.body);
+  const data = req.body;
+  res.json({
+    status: "success",
+    latitude: data.lat,
+    longtitude: data.lng
+  });
+});
