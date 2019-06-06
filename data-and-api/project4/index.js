0a432f77d2abc5713fe305caacb525ab0768471f
HR <hrui1990@gmail.com>
2019-06-05 23:43:20 -0500

    update project 4

--- data-and-api/project4/index.js
+++ data-and-api/project4/index.js
@@ -1,15 +1,25 @@
 const express = require("express");
-const app = express();
+const Datastore = require("nedb");
 
+const app = express();
 app.listen(3000, () => console.log("Listening on port 3000"));
 
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
+const database = new Datastore("database.db");
+database.loadDatabase();
+
 app.post("/api", (req, res) => {
   const data = req.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+
+  database.insert(data);
+
   res.json({
     status: "success",
+    timestamp: timestamp,
     latitude: data.lat,
     longtitude: data.lng
   });
